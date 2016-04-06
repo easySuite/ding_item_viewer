afd05a59b4ba5999b11148d227b2838749e4ff2a
MM <mm@inlead.dk>
2016-04-06 19:00:22 +0300

    DDBEASY-794 - Fix ding item carousel behaviour.

--- js/ding_item_viewer.js
+++ js/ding_item_viewer.js
@@ -4,20 +4,56 @@
    */
   $(document).ready(function() {
     $('.ding-item-viewer').each(function(i, e) {
-      var viewer_wrapper = this;
-      var carousel_hash = $(e).find('div[id^="ding_item_viewer-"]').attr('ding-item-viewer-hash');
-      var carousel_wrapper = $(e).children('div[id^="ding_item_viewer-"]');
-      var carousel_tabs = $(e).find('ul.ding-item-viewer-list-tabs');
-      var tabs_index = $(carousel_tabs).children().length - 1;
-      var autoplay = $(carousel_wrapper).attr('ding-item-viewer-autoplay') * 1000;
-      var copy_holder = $('.ding-item-viewer-small');
+      var autoplay = $(e).attr('ding-item-viewer-autoplay') * 1000;
+      var tabs = $(e).find('ul.ding-item-viewer-list-tabs');
+      var tabs_index = $(tabs).children().length - 1;
       var index = 0;
 
-      // Change small carousel item on big when carousel library init.
-      $(carousel_wrapper).on('init', function(event, slick) {
-        var current_item_id = $(this).find('[data-slick-index=' + slick.currentSlide + ']').attr('carousel-item');
-        var current_item = $('#' + carousel_hash + ' div[carousel-item="' + current_item_id + '" ]');
-        var current_item_big = $(viewer_wrapper).find('.ding-item-viewer-big [carousel-item="' + current_item_id + '"]');
+      // Init carousel on page load and tab change.
+      function show_carousel(index) {
+        var current_tab = '#tab-' + index;
+        var carousel_wrapper = $(e).find(current_tab + ' div.carousel-items');
+
+        $(current_tab).show();
+        $(carousel_wrapper).show();
+
+        // Change small carousel item on big when carousel library init.
+        $(carousel_wrapper).on('init', function(event, slick) {
+          change_item_size_to_big(this, index, slick.currentSlide);
+        });
+
+        // Init carousel
+        $(carousel_wrapper).slick({
+          arrows: true,
+          centerMode: true,
+          centerPadding: '50px',
+          slidesToShow: 5,
+          slidesToScroll: 1
+        });
+
+        // Change small carousel item on big on carousel change.
+          $(carousel_wrapper).on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+            change_next_item_to_big(this, index, currentSlide, nextSlide);
+        });
+      }
+
+      // Hide carousel on tab change.
+      function hide_carousel(index) {
+        var current_tab = '#tab-' + index;
+        var carousel_wrapper = $(e).find(current_tab + ' div.carousel-items');
+        var current_slide = $(carousel_wrapper).slick('slickCurrentSlide');
+
+        reset_item_size($(carousel_wrapper), index, current_slide);
+        $(carousel_wrapper).slick('unslick');
+      }
+
+      // Change item size from small to big on carousel init.
+      function change_item_size_to_big(carousel, tab_index, slick_slide) {
+        var current_tab = '#tab-' + tab_index;
+        var current_item_id = $(carousel).find('[data-slick-index=' + slick_slide + ']').attr('carousel-item');
+        var current_item = $(carousel).find('div[carousel-item="' + current_item_id + '"]');
+        var current_item_big = $(current_tab).find('.carousel-items-big [carousel-item="' + current_item_id + '"]');
+        var copy_holder = $(current_tab + ' .ding-item-viewer-small');
 
         // Copy current carousel item.
         copy_holder.html($(current_item).html());
@@ -25,22 +61,15 @@
         copy_holder.children().hide();
 
         $(current_item).html($(current_item_big).html());
-      });
+      }
 
-      // Init carousel
-      $(carousel_wrapper).slick({
-        arrows: true,
-        centerMode: true,
-        centerPadding: '50px',
-        slidesToShow: 5,
-        slidesToScroll: 1
-      });
-
-      // Change small carousel item on big on carousel change.
-      $(carousel_wrapper).on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+      // Change current slide size to small and next slide to big on carousel slide.
+      function change_next_item_to_big(carousel, tab_index, cur_slide, next_slide) {
         // Replace big slide with small.
-        var current_item_id = $(this).find('[data-slick-index=' + currentSlide + ']').attr('carousel-item');
-        var current_item = $('#' + carousel_hash + ' div[carousel-item="' + current_item_id + '"]');
+        var current_tab = '#tab-' + tab_index;
+        var current_item_id = $(carousel).find('[data-slick-index=' + cur_slide + ']').attr('carousel-item');
+        var current_item = $(carousel).find('div[carousel-item="' + current_item_id + '"]');
+        var copy_holder = $(current_tab + ' .ding-item-viewer-small');
         var current_item_small = copy_holder.html();
 
         copy_holder.empty();
@@ -48,9 +77,9 @@
         $(current_item).children('div').show();
 
         //Replace small slide with big.
-        var next_item_id = $(this).find('[data-slick-index=' + nextSlide + ']').attr('carousel-item');
-        var next_item = $('#' + carousel_hash + ' div[carousel-item="' + next_item_id + '"]');
-        var next_item_big = $(viewer_wrapper).find('.ding-item-viewer-big [carousel-item="' + next_item_id + '"]');
+        var next_item_id = $(carousel).find('[data-slick-index=' + next_slide + ']').attr('carousel-item');
+        var next_item = $(carousel).find('div[carousel-item="' + next_item_id + '"]');
+        var next_item_big = $(current_tab).find('.carousel-items-big [carousel-item="' + next_item_id + '"]');
 
         copy_holder.html($(next_item).html());
         $(next_item).empty();
@@ -58,37 +87,45 @@
 
         $(next_item).html($(next_item_big).html());
         $(next_item).show();
-      });
+      }
 
-      update_carousel(false, false);
+      // Reset current item size on tab change.
+      function reset_item_size(carousel, tab_index, slick_slide) {
+        // Replace big slide with small.
+        var current_tab = '#tab-' + tab_index;
+        var current_item_id = $(carousel).find('[data-slick-index=' + slick_slide + ']').attr('carousel-item');
+        var current_item = $(carousel).find('div[carousel-item="' + current_item_id + '"]');
+        var copy_holder = $(current_tab + ' .ding-item-viewer-small');
+        var current_item_small = copy_holder.html();
+
+        copy_holder.empty();
+        $(current_item).html(current_item_small);
+        $(current_item).children('div').show();
+      }
+
+      show_carousel(0);
 
       // Stop tab switching if mouse enters carousel.
       var is_active = true;
-      $(carousel_wrapper).mouseenter(function() {
+      $(e).mouseenter(function() {
         is_active = false;
       }).mouseleave(function() {
         is_active = true;
       });
 
-      $("#" + carousel_hash).find("div[data-slick-index=0]").show();
-
-      // Switch tabs by timer.
-      setInterval(function() {
-        if (is_active) {
-          if (index == tabs_index) {
-            update_carousel(false, false);
+      if (autoplay != 0) {
+        // Switch tabs by timer.
+        setInterval(function() {
+          if (is_active) {
+            if (index == tabs_index) {
+              update_carousel(false, false);
+            }
+            else {
+              update_carousel(true, false);
+            }
           }
-          else {
-            update_carousel(true, false);
-          }
-        }
-      }, autoplay);
-
-      // Switch tabs on click by it.
-      $(carousel_tabs).find('li').on('click', function(event) {
-        event.preventDefault();
-        update_carousel(false, $(this).attr('tab-index'));
-      });
+        }, autoplay);
+      }
 
       /**
        * Function updates carousel on tab switching.
@@ -108,11 +145,18 @@
           index = (increment) ? index + 1 : 0;
         }
 
-        $(carousel_tabs).find('li.index-' + prev_index).removeClass('active');
-        $(carousel_tabs).find('li.index-' + index).addClass('active');
-        $(carousel_wrapper).slick('slickUnfilter');
-        $(carousel_wrapper).slick('slickFilter', '.index-' + index);
+        $(tabs).find('li.index-' + prev_index).removeClass('active');
+        $(tabs).find('li.index-' + index).addClass('active');
+        $(e).children('div#tab-' + prev_index).hide();
+        hide_carousel(prev_index);
+        show_carousel(index);
       }
+
+      // Switch tabs on click by it.
+      $(tabs).find('li').on('click', function(event) {
+        event.preventDefault();
+        update_carousel(false, $(this).attr('tab-index'));
+      });
     });
   });
 })(jQuery);
