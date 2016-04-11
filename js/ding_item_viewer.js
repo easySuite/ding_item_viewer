394570e0b47aad0f969f37bdea1f9330047562fe
MM <mm@inlead.dk>
2016-04-11 12:10:59 +0300

    DDBEASY-794 - Rewrite some code.

--- js/ding_item_viewer.js
+++ js/ding_item_viewer.js
@@ -4,6 +4,7 @@
    */
   $(document).ready(function() {
     $('.ding-item-viewer').each(function(i, e) {
+      // var autoplay = 1000 * 1000;
       var autoplay = $(e).attr('ding-item-viewer-autoplay') * 1000;
       var tabs = $(e).find('ul.ding-item-viewer-list-tabs');
       var tabs_index = $(tabs).children().length - 1;
@@ -17,11 +18,6 @@
         $(current_tab).show();
         $(carousel_wrapper).show();
 
-        // Change small carousel item on big when carousel library init.
-        $(carousel_wrapper).on('init', function(event, slick) {
-          change_item_size_to_big(this, index, slick.currentSlide);
-        });
-
         // Init carousel
         $(carousel_wrapper).slick({
           arrows: true,
@@ -31,9 +27,12 @@
           slidesToScroll: 1
         });
 
+        change_item_size_to_big(index, 0);
+
         // Change small carousel item on big on carousel change.
-          $(carousel_wrapper).on('beforeChange', function(event, slick, currentSlide, nextSlide) {
-            change_next_item_to_big(this, index, currentSlide, nextSlide);
+        $(carousel_wrapper).on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+          event.preventDefault();
+          change_next_item_to_big(this, index, currentSlide, nextSlide);
         });
       }
 
@@ -43,22 +42,24 @@
         var carousel_wrapper = $(e).find(current_tab + ' div.carousel-items');
         var current_slide = $(carousel_wrapper).slick('slickCurrentSlide');
 
-        reset_item_size($(carousel_wrapper), index, current_slide);
+        reset_item_size(index, current_slide);
         $(carousel_wrapper).slick('unslick');
       }
 
       // Change item size from small to big on carousel init.
-      function change_item_size_to_big(carousel, tab_index, slick_slide) {
+      function change_item_size_to_big(tab_index, slick_slide) {
+        // console.log(slick_slide)
         var current_tab = '#tab-' + tab_index;
-        var current_item_id = $(carousel).find('[data-slick-index=' + slick_slide + ']').attr('carousel-item');
-        var current_item = $(carousel).find('div[carousel-item="' + current_item_id + '"]');
+        var current_item_id = $(current_tab).find('div.carousel-items [data-slick-index=' + slick_slide + ']').attr('carousel-item');
+        var current_item = $(current_tab).find('div.carousel-items  div[carousel-item="' + current_item_id + '"]');
         var current_item_big = $(current_tab).find('.carousel-items-big [carousel-item="' + current_item_id + '"]');
         var copy_holder = $(current_tab + ' .ding-item-viewer-small');
 
-        // Copy current carousel item.
-        copy_holder.html($(current_item).html());
+        $(copy_holder).empty();
+        $(copy_holder).html($(current_item).html());
+
         $(current_item).empty();
-        copy_holder.children().hide();
+        $(copy_holder).hide();
 
         $(current_item).html($(current_item_big).html());
       }
@@ -67,40 +68,31 @@
       function change_next_item_to_big(carousel, tab_index, cur_slide, next_slide) {
         // Replace big slide with small.
         var current_tab = '#tab-' + tab_index;
-        var current_item_id = $(carousel).find('[data-slick-index=' + cur_slide + ']').attr('carousel-item');
-        var current_item = $(carousel).find('div[carousel-item="' + current_item_id + '"]');
+        var current_item_id = $(current_tab).find('div.carousel-items [data-slick-index=' + cur_slide + ']').attr('carousel-item');
+        var current_item = $(current_tab).find('div.carousel-items  div[carousel-item="' + current_item_id + '"]');
         var copy_holder = $(current_tab + ' .ding-item-viewer-small');
-        var current_item_small = copy_holder.html();
 
-        copy_holder.empty();
-        $(current_item).html(current_item_small);
-        $(current_item).children('div').show();
+        $(current_item).empty();
+        $(current_item).html($(copy_holder).html());
+        $(current_item).show();
+        $(copy_holder).empty();
 
         //Replace small slide with big.
-        var next_item_id = $(carousel).find('[data-slick-index=' + next_slide + ']').attr('carousel-item');
-        var next_item = $(carousel).find('div[carousel-item="' + next_item_id + '"]');
-        var next_item_big = $(current_tab).find('.carousel-items-big [carousel-item="' + next_item_id + '"]');
-
-        copy_holder.html($(next_item).html());
-        $(next_item).empty();
-        copy_holder.hide();
-
-        $(next_item).html($(next_item_big).html());
-        $(next_item).show();
+        change_item_size_to_big(tab_index, next_slide);
       }
 
       // Reset current item size on tab change.
-      function reset_item_size(carousel, tab_index, slick_slide) {
+      function reset_item_size(tab_index, slick_slide) {
         // Replace big slide with small.
         var current_tab = '#tab-' + tab_index;
-        var current_item_id = $(carousel).find('[data-slick-index=' + slick_slide + ']').attr('carousel-item');
-        var current_item = $(carousel).find('div[carousel-item="' + current_item_id + '"]');
+        var current_item_id = $(current_tab).find('div.carousel-items [data-slick-index=' + slick_slide + ']').attr('carousel-item');
+        var current_item = $(current_tab).find('div.carousel-items  div[carousel-item="' + current_item_id + '"]');
         var copy_holder = $(current_tab + ' .ding-item-viewer-small');
-        var current_item_small = copy_holder.html();
 
-        copy_holder.empty();
-        $(current_item).html(current_item_small);
-        $(current_item).children('div').show();
+        $(current_item).empty();
+        $(current_item).html($(copy_holder).html());
+        $(current_item).show();
+        $(copy_holder).empty();
       }
 
       show_carousel(0);
